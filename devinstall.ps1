9be57425e5193ffa4eb2506ea88b2ec783589aa4
Force Charlie <ipvb@qq.com>
2018-03-29 11:32:34 +0800

    init

---
+++ devinstall.ps1
@@ -0,0 +1,48 @@
+#!/usr/bin/env pwsh
+
+$toolslockfile = $PSScriptRoot + [System.IO.Path]::DirectorySeparatorChar + "devinstall.lock.json"
+if (!(Test-Path $toolsfile)) {
+    Write-Host -ForegroundColor Red "Not Found $toolsfile"
+    Exit 1
+}
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 # Force use TLS 1.2
+$InternalUA = [Microsoft.PowerShell.Commands.PSUserAgent]::Chrome
+
+Function DownloadFile {
+    param(
+        [String]$Uri,
+        [String]$Path
+    )
+    Write-Host "Download URL: $Uri"
+    try {
+        Invoke-WebRequest -Uri $Uri -OutFile $Path -UserAgent $InternalUA -UseBasicParsing
+    }
+    catch {
+        Write-Host -ForegroundColor Red "Download error: $_"
+        return $false
+    }
+    return $true
+}
+
+$toolslocked = Get-Content $toolslockfile| ConvertFrom-Json
+$newlocked = @{}
+$cmake_major = 3
+$cmake_minor = 11
+$cmake_patchver = 2
+$cmake_version = "$cmake_major.$cmake_minor.$cmake_patchver"
+
+if ($toolslocked.cmake -ne $cmake_version) {
+    # Download cmake and install.
+    $cmakeurl = "https://cmake.org/files/v$cmake_major.$cmake_minor/cmake-$cmake_major.$cmake_minor.$cmake_pathver-Linux-x86_64.sh"
+    if (DownloadFile -Uri $cmakeurl -Path "/tmp/cmake.sh") {
+        chmod +x "/tmp/cmake.sh"
+        sudo "/tmp/cmake.sh" --prefix=/usr/local --skip-license
+        $newlocked["cmake"] = $cmake_version
+    }
+}
+if ($newlocked["cmake"] -eq $null) {
+    $newlocked["cmake"] = $toolslocked.cmake
+}
+
+ConvertTo-Json $newlocked |Out-File -Force -FilePath $toolslockfile
