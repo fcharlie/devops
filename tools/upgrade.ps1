b34f844de48198459c874682fb8cee682691eda4
Force Charlie <ipvb@qq.com>
2018-03-30 19:07:20 +0800

    fix success

--- tools/upgrade.ps1
+++ tools/upgrade.ps1
@@ -5,8 +5,7 @@ Import-Module -Name "$Devroot/modules/Download"
 Import-Module -Name "$Devroot/modules/Process"
 Import-Module -Name "$Devroot/modules/Utils"
 
-[int]$successed = 0
-[int]$total = 0
+
 Function TryUpgradePorts {
     param(
         [String]$Name,
@@ -16,29 +15,38 @@ Function TryUpgradePorts {
     $cjson = Get-Content "$Devroot/ports/$Name/config.json" -ErrorAction SilentlyContinue|ConvertFrom-Json -ErrorAction SilentlyContinue
     if ($cjson -eq $null) {
         Write-Host "Invalid port: $Name."
-        return 
+        return -1
     }
     if ($cjson.version -eq $Version) {
-        return 
+        return -1
     }
-    $total++
+    Write-Host "try to upgrade $Name to $($cjson.version)"
     &"$Root/ports/$Name/$Name.ps1"
     if ($LASTEXITCODE -eq 0) {
-        $successed++
         Write-Host -ForegroundColor  Green "upgrade: $Name success."
+        return 0
     }
     else {
         Write-Host -ForegroundColor  Red "upgrade: $Name failed."
+        return 1
     }
 }
 
-
+[int]$success = 0
+[int]$total = 0
 Get-ChildItem -Path "$Devroot/locks/*.lock.json" |ForEach-Object {
 
     $cj = Get-Content $_.FullName |ConvertFrom-Json
     $version = $cj.version
     $name = $_.BaseName.Split(".lock")[0]
-    TryUpgradePorts -Name $name -Root $Devroot -Version $version
+    $ret = TryUpgradePorts -Name $name -Root $Devroot -Version $version
+    if ($ret -eq 1) {
+        $total++
+    }
+    elseif ($ret -eq 0) {
+        $total++
+        $success++
+    }
 }
 
-Write-Host "devops: upgrade tools/libs, success $successed/$total"
+Write-Host "devops: upgrade tools/libs, success $success/$total"
