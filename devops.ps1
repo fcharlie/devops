684d82cc8bef438c9a5a9b822802a6c532506889
Force Charlie <ipvb@qq.com>
2018-03-30 11:36:41 +0800

    devops

---
+++ devops.ps1
@@ -0,0 +1,19 @@
+#!/usr/bin/env pwsh
+## devops.ps1 list
+## devops.ps1 version
+## devops.ps1 install golang
+
+
+Function DevopsList {
+    param(
+        [String]$Devroot
+    )
+    Write-Host -ForegroundColor Green "devops tools, found ports:"
+    Get-ChildItem -Path "$Devroot/ports" |ForEach-Object {
+        $cj = Get-Content "$($_.FullName)/config.json" |ConvertFrom-Json
+        $version = $cj.version
+        Write-Host "$($_.BaseName)`t$version`t$($cj.desc)"
+    }
+}
+
+DevopsList -Devroot $PSScriptRoot
---
+++ ports/nodejs/nodejs.ps1
@@ -0,0 +1,3 @@
+#!/usr/bin/env nodejs
+
+# nodejs url: $url/v$version/node-v$version-linux-x64.tar.xz
