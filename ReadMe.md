be2857bfcee2c9a4bb1c337eac2b5ee8a177f241
Force Charlie <ipvb@qq.com>
2018-03-30 18:23:21 +0800

    devops

--- ReadMe.md
+++ ReadMe.md
@@ -8,6 +8,20 @@ devops is some tools install or compile script set.
 
 This tool need install PowerShell Core,SEE: [Package Installation Instructions](https://github.com/PowerShell/PowerShell/blob/master/docs/installation/linux.md).
 
+## Usage
+
+```txt
+Devops utilies 1.0
+Usage: devops cmd args
+       list        list installed tools/libs
+       search      search ported tools/libs
+       install     install tools or libs
+       version     print devops version and exit
+       config      config your secure repository
+       help        print help message
+
+```
+
 ## Copyright
 
 Licensed under MIT
--- devops.ps1
+++ devops.ps1
@@ -2,9 +2,40 @@
 ## devops.ps1 list
 ## devops.ps1 version
 ## devops.ps1 install golang
+## devops.ps1 
 
+Function PrintUsage {
+    Write-Host "Devops utilies 1.0
+Usage: devops cmd args
+       list        list installed tools/libs
+       search      search ported tools/libs
+       install     install tools or libs
+       version     print devops version and exit
+       config      config your secure repository
+       help        print help message
+"
+}
 
+# list
 Function DevopsList {
+    param(
+        [String]$Devroot
+    )
+    if (!(Test-Path $Devroot)) {
+        Write-Host "devops: no tools installed."
+        return
+    }
+    Write-Host -ForegroundColor Green "devops tools, found installed tools:"
+    Get-ChildItem -Path "$Devroot/locks/*.lock.json" |ForEach-Object {
+        $cj = Get-Content $_.FullName |ConvertFrom-Json
+        $version = $cj.version
+        $name = $_.BaseName.Split(".lock")[0]
+        Write-Host "$name`t$version`t$($cj.prefix)"
+    }
+}
+
+# search
+Function DevopsSearch {
     param(
         [String]$Devroot
     )
@@ -16,4 +47,49 @@ Function DevopsList {
     }
 }
 
-DevopsList -Devroot $PSScriptRoot
+if ($args.Count -eq 0) {
+    PrintUsage
+    exit 0
+}
+
+$subcmd = $args[0]
+
+switch ($subcmd) {
+    "list" {
+        DevopsList -Devroot $PSScriptRoot
+    }
+    "search" {
+        DevopsSearch -Devroot $PSScriptRoot
+    }
+    "install" {
+        if ($args.Count -lt 2) {
+            Write-Host -ForegroundColor Red "devops install missing argument, example: devops install golang"
+            exit 1
+        }
+        $portx = $args[1]
+        $portfile = $PSScriptRoot + "/ports/$portx/$portx.ps1"
+        if (!(Test-Path $portfile)) {
+            Write-Host -ForegroundColor Red -NoNewline "$portx "
+            Write-Host "has not been ported to devops,
+You can add your favorite tools to devops!"
+            exit 1
+        }
+        &$portfile
+        exit $LASTEXITCODE
+    }
+    "version" {
+        Write-Host "devops: 1.0"
+    }
+    "help" {
+        PrintUsage
+        exit 0
+    }
+    "--help" {
+        PrintUsage
+        exit 0
+    }
+    Default {
+        Write-Host -ForegroundColor Red "unsupported command '$xcmd' your can run devops help -a"
+        exit 1
+    }
+}
