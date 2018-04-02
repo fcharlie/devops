6c74e3acec252bda50139a19a07c1e0717912535
Force Charlie <ipvb@qq.com>
2018-04-02 10:33:33 +0800

    devops upgrade fix

--- tools/upgrade.ps1
+++ tools/upgrade.ps1
@@ -5,47 +5,29 @@ Import-Module -Name "$Devroot/modules/Download"
 Import-Module -Name "$Devroot/modules/Process"
 Import-Module -Name "$Devroot/modules/Utils"
 
-
-Function TryUpgradePorts {
-    param(
-        [String]$Name,
-        [String]$Root,
-        [String]$Version
-    )
-    $cjson = Get-Content "$Devroot/ports/$Name/config.json" -ErrorAction SilentlyContinue|ConvertFrom-Json -ErrorAction SilentlyContinue
-    if ($cjson -eq $null) {
-        Write-Host "Invalid port: $Name."
-        return -1
-    }
-    if ($cjson.version -eq $Version) {
-        return -1
-    }
-    Write-Host "try to upgrade $Name to $($cjson.version)"
-    &"$Root/ports/$Name/$Name.ps1"
-    if ($LASTEXITCODE -eq 0) {
-        Write-Host -ForegroundColor  Green "upgrade: $Name success."
-        return 0
-    }
-    else {
-        Write-Host -ForegroundColor  Red "upgrade: $Name failed."
-        return 1
-    }
-}
-
 [int]$success = 0
 [int]$total = 0
 Get-ChildItem -Path "$Devroot/locks/*.lock.json" |ForEach-Object {
-
-    $cj = Get-Content $_.FullName |ConvertFrom-Json
-    $version = $cj.version
+    $lkv = Get-Content $_.FullName |ConvertFrom-Json
+    $version = $lkv.version
     $name = $_.BaseName.Split(".lock")[0]
-    $ret = TryUpgradePorts -Name $name -Root $Devroot -Version $version
-    if ($ret -eq 1) {
-        $total++
+    $njson = Get-Content "$Devroot/ports/$Name/config.json" -ErrorAction SilentlyContinue|ConvertFrom-Json -ErrorAction SilentlyContinue
+    if ($njson -eq $null -or $njson.version -eq $null) {
+        Write-Host "Invalid port: $Name."
+        return
     }
-    elseif ($ret -eq 0) {
-        $total++
+    if ($njson.version -eq $version) {
+        #Write-Host "$name is already up to date"
+        return
+    }
+    $total++
+    &"$Devroot/ports/$name/$name.ps1"
+    if ($LASTEXITCODE -eq 0) {
+        Write-Host -ForegroundColor  Green "devops: upgrade $name success."
         $success++
+    }
+    else {
+        Write-Host -ForegroundColor  Red "devops: upgrade $name failed."
     }
 }
 
